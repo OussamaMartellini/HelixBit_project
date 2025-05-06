f3a8251e2926e7c73410ba7d4d239c111408ef1a
Oussama Martellini <oussamamartellini@gmail.com>
2025-05-06 18:25:05 +0200

    .

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Sidebar from "../components/Sidebar";
-import NavbarMobile from "../components/navbarMobile";
+import NavbarMobile from "../components/NavbarMobile";
 import GenresDropdown from "../components/GenresDropdown";
 function Layout() {
     return (
